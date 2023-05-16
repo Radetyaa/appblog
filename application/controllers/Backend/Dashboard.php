747262ce77978212d0242c86de7988f79dc7fa5e
Radetyaa <bimaadit240100@gmail.com>
2023-05-16 16:11:45 +0700

    tambah template dan login

--- application/controllers/Backend/Dashboard.php
+++ application/controllers/Backend/Dashboard.php
@@ -1,19 +1,16 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-use Jenssegers\Blade\Blade;
-class Dashboard extends CI_Controller {
+
+class Dashboard extends MY_Controller 
+{
 
 	public function index()
 	{
-		$this->load->view('template/backend/theme');
+		view('backend/Dashboard/index');
 	}
-	public function tes()
+	public function table()
 	{
-		$views = VIEWPATH;
-		$cache = APPPATH . 'cache';
-		$blade = new Blade($views, $cache);
-
-		echo $blade->make('template/backend/tes', [])->render();
+		view('backend/Dashboard/table');
 	}
 }
 
